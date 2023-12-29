be36f6e13fc8fdeafd6fff0dd4a7e9d56e15526c
bhsunilvarma13 <mailme4loops@gmail.com>
2023-12-29 19:48:02 +0530

    Check-Point #1

---
+++ backend/src/managers/IoManager.ts
@@ -0,0 +1,18 @@
+import { Server } from "socket.io";
+import * as http from "http";
+
+const server = http.createServer();
+
+export class IoManager {
+  private static io: Server;
+
+  public static getIo() {
+    if (!this.io) {
+      const io = new Server(server);
+
+      this.io = io;
+    }
+
+    return this.io;
+  }
+}
---
+++ backend/src/managers/QuizManager.ts
@@ -0,0 +1,18 @@
+import { Quiz } from "../Quiz";
+import { IoManager } from "./IoManager";
+
+export class QuizManager {
+  private quizes: Quiz[];
+
+  constructor() {
+    this.quizes = [];
+  }
+
+  public start(roomId: string) {
+    const io = IoManager.getIo();
+
+    const quiz = this.quizes.find((x) => x.roomId === roomId);
+
+    quiz.start();
+  }
+}
